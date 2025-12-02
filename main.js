90d57db851d77d7669a50c450a50acb4e02bfb1d
princekumarobra <PRINCEKUMAR73678852@GMAIL.COM>
2025-12-03 02:25:18 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.querySelector(".nav-toggle");
+    const nav = document.querySelector(".main-nav");
+
+    if (toggle && nav) {
+        toggle.addEventListener("click", function () {
+            nav.classList.toggle("open");
+        });
+    }
+  });
+</script>
